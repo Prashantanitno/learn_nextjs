5ac3187b9b102f065acdcf9f106c31c20bb8083e
Prashantanitno <aditya555antino@gmail.com>
2024-02-29 13:08:05 +0530

    review links exercise

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -1,9 +1,18 @@
+import Link from "next/link";
 import React from "react";
 
 const ReviewsPage = () => {
   return (
     <div>
       <h1>Reviews Page</h1>
+      <ul>
+        <li>
+          <Link href={"/reviews/hollow-knight"}>Hollow Knight</Link>
+        </li>
+        <li>
+          <Link href={"/reviews/stardew-valley"}>Stardew Valley</Link>
+        </li>
+      </ul>
     </div>
   );
 };
